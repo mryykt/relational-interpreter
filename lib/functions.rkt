322a77077c2ec5c4f0928a8f392c7e1fb6c111a1
mryykt <avrpic9@gmail.com>
2025-10-23 10:28:34 +0900

    feat: ヘルパーマクロ

--- lib/functions.rkt
+++ lib/functions.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require syntax/parse/define)
 (require minikanren)
 (require minikanren/numbers)
 (require "j.rkt")
@@ -29,35 +30,44 @@
                        (app (app (var g) (car (var xs)))
                             (app (app (app (var f) (var g)) (var init)) (cdr (var xs)))))))))
 
+(define (symbol-trim-last sym)
+  (let* ([s (symbol->string sym)]
+         [trimmed (substring s 0 (max 0 (sub1 (string-length s))))])
+    (string->symbol trimmed)))
+
+(define-syntax make-program
+  (syntax-rules ()
+    [(_ c) c]
+    [(_ f c)
+     `(let ,(symbol-trim-last 'f)
+        ,f
+        c)]
+    [(_ f1 f2 ...)
+     `(let ,(symbol-trim-last 'f1)
+        ,f1
+        ,(make-program f2 ...))]))
+
 (define (run-test)
   (test "cons"
         (run 1
              (q)
-             (evalo `(let cons
-                       ,consf
-                       (app (app (var cons) (num ,(build-num 1))) ,(list-c 2 3)))
-                    q))
+             (evalo (make-program consf (app (app (var cons) (num ,(build-num 1))) ,(list-c 2 3))) q))
         `(,(list-v 1 2 3)))
   (test "foldl"
         (run 1
              (q)
-             (evalo `(let foldl
-                       ,foldlf
-                       (let flip
-                         ,flipf
-                         (app (app (app (var foldl)
-                                        (app (var flip) (lam x (lam y (cons (var x) (var y))))))
-                                   (list ()))
-                              ,(list-c 1 2 3))))
+             (evalo (make-program foldlf
+                                  flipf
+                                  consf
+                                  (app (app (app (var foldl) (app (var flip) (var cons))) (list ()))
+                                       ,(list-c 1 2 3)))
                     q))
         `(,(list-v 3 2 1)))
   (test "foldr"
         (run 1
              (q)
-             (evalo `(let foldr
-                       ,foldrf
-
-                       (app (app (app (var foldr) (lam x (lam y (cons (var x) (var y))))) (list ()))
-                            ,(list-c 1 2 3)))
+             (evalo (make-program foldrf
+                                  consf
+                                  (app (app (app (var foldr) (var cons)) (list ())) ,(list-c 1 2 3)))
                     q))
         `(,(list-v 1 2 3))))
