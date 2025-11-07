06db733a5f5d7f9b48cd818c10daa78e11e7b230
mryykt <avrpic9@gmail.com>
2025-11-07 22:29:30 +0900

    feat: compose，map関数

--- lib/functions.rkt
+++ lib/functions.rkt
@@ -8,14 +8,18 @@
 
 (provide consf
          flipf
+         composef
          foldlf
          foldrf
+         mapf
          make-program
          run-test)
 
 (define consf '(lam x (lam y (cons (var x) (var y)))))
 
 (define flipf '(lam f (lam x (lam y (app (app (var f) (var y)) (var x))))))
+
+(define composef '(lam f (lam g (lam x (app (var f) (app (var g) (var x)))))))
 
 (define foldlf
   '(fix f
@@ -36,6 +40,14 @@
                        (var init)
                        (app (app (var g) (car (var xs)))
                             (app (app (app (var f) (var g)) (var init)) (cdr (var xs)))))))))
+
+(define mapf
+  '(fix f
+        g
+        (lam xs
+             (ifz (= (var xs) (list ()))
+                  (list ())
+                  (cons (app (var g) (car (var xs))) (app (app (var f) (var g)) (cdr (var xs))))))))
 
 (define (symbol-trim-last sym)
   (let* ([s (symbol->string sym)]
@@ -65,6 +77,15 @@
    "cons"
    (run 1 (q) (evalo (make-program consf ,(apps (var cons) (num ,(build-num 1)) ,(list-c 2 3))) q))
    `(,(list-v 1 2 3)))
+  (test
+   "compose"
+   (run 1
+        (q)
+        (evalo (make-program
+                composef
+                ,(apps (var compose) (lam x (car (var x))) (lam x (cdr (var x))) ,(list-c 1 2 3)))
+               q))
+   `(,(build-num 2)))
   (test "foldl"
         (run 1
              (q)
@@ -81,4 +102,11 @@
         (q)
         (evalo (make-program foldrf consf ,(apps (var foldr) (var cons) (list ()) ,(list-c 1 2 3)))
                q))
-   `(,(list-v 1 2 3))))
+   `(,(list-v 1 2 3)))
+  (test "map"
+        (run 1
+             (q)
+             (evalo (make-program mapf
+                                  ,(apps (var map) (lam x (cons (var x) (list ()))) ,(list-c 1 2 3)))
+                    q))
+        `(,(list-v '(1) '(2) '(3)))))
