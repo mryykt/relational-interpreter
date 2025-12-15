48f7d043955ee4aa4aa63cf0797822a0c872b8b5
mryykt <avrpic9@gmail.com>
2025-12-15 17:58:42 +0900

    feat: タイムアウトによる例外をキャッチする

--- lib/test-check.rkt
+++ lib/test-check.rkt
@@ -4,13 +4,17 @@
 
 (define test-failed (make-parameter #f))
 
+(define timeout-second 300)
+
 (define-syntax test
   (syntax-rules ()
     [(_ title tested-expression expected-result)
      (begin
        (printf "Testing ~s\n" title)
        (let* ([expected expected-result]
-              [produced (call-with-limits 300 #f (lambda () (time tested-expression)))])
+              [produced (with-handlers ([exn:fail:resource?
+                                         (λ (e) (displayln (format "Timeout in ~s" timeout-second)))])
+                          (call-with-limits timeout-second #f (λ () (time tested-expression))))])
          (or (equal? expected produced)
              (begin
                (test-failed #t)
