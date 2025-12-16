1d5f046ad9dd43ed74e647f19a30e8357d81395e
mryykt <avrpic9@gmail.com>
2025-12-16 11:41:48 +0900

    feat: 合わせた

--- lib/combinator-wit-type-and-uniq-appearance.rkt
+++ lib/combinator-wit-type-and-uniq-appearance.rkt
@@ -21,6 +21,8 @@
          [(app ,u ,v)
           (fresh (t0 env^^) (combinator u env env^^ `(fun ,t0 ,t)) (combinator v env^^ env^ t0))]
          [(var ,u) (symbolo u) (lookup-firsto u env t) (rembero u env env^)]
+         [(num ()) (== t 'int)]
+         [(num (1)) (== t 'int)]
          [() (fresh (et) (== t `(list ,et)) (== env env^))]))
 
 (defrel (typed-helpero ne nt)
@@ -29,52 +31,76 @@
 (define-syntax synthesis
   (syntax-rules ()
     [(_ n (q) t (input ...) output)
-     (run n
-          (q)
-          (fresh (env tenv res)
-                 (mapo typed-helpero all-functions-list tenv)
-                 (combinator q tenv res t)
-                 (evalo (with-all-functions (apps ,q input ...)) output)))]
+     (map unparser
+          (run n
+               (q)
+               (fresh (env tenv res)
+                      (mapo typed-helpero all-functions-list tenv)
+                      (combinator q tenv res t)
+                      (evalo (with-all-functions (apps ,q input ...)) output))))]
     [(_ n (q) t (function ...) (input ...) output)
-     (let ([env `((,(symbol-trim-last 'function) . ,function) ...)])
-       (run n
-            (q)
-            (fresh (tenv res)
-                   (mapo typed-helpero env tenv)
-                   (combinator q tenv res t)
-                   (evalo (with-functions env (apps ,q input ...)) output))))]))
+     (let ([env (append `((,(symbol-trim-last 'function) . ,function) ...) all-basic-functions)])
+       (map unparser
+            (run n
+                 (q)
+                 (fresh (tenv res)
+                        (mapo typed-helpero env tenv)
+                        (combinator q tenv res t)
+                        (evalo (with-functions env (apps ,q input ...)) output)))))]))
 
 (define (run-test)
-  (test
-   "reverse"
-   (synthesis 1 (q) '(fun (list int) (list int)) (foldlf flipf consf) (,(list-c 1 2)) (list-v 2 1))
-   `(,(parser '(foldl (flip cons) ()))))
+  (test "reverse"
+        (synthesis 1 (q) '(fun (list int) (list int)) (foldlf) (,(list-c 1 2)) (list-v 2 1))
+        '((foldl (flip cons) ())))
   (test "append"
         (synthesis 1
                    (q)
                    '(fun (list int) (fun (list int) (list int)))
-                   (foldrf consf flipf)
+                   (foldrf)
                    (,(list-c 1 2) ,(list-c 3 4))
                    (list-v 1 2 3 4))
-        `(,(parser '(flip (foldr cons)))))
-  (test "concat"
-        (synthesis 1
-                   (q)
-                   '(fun (list (list int)) (list int))
-                   (foldrf foldlf flipf consf)
-                   (,(list-c '(1 2) '(3 4)))
-                   (list-v 1 2 3 4))
-        `(,(parser '(foldl (flip (foldr cons)) ()))))
-  (let ([addf '(lam x (lam y ((var x) + (var y))))]
-        [0f '(num ())])
-    (test "sum"
-          (synthesis 1 (q) '(fun (list int) int) (foldlf addf 0f) (,(list-c 1 2 3)) (build-num 6))
-          `(,(parser '(foldl add |0|)))))
+        '((flip (foldr cons))))
+  ;   (test "concat"
+  ;         (synthesis 1
+  ;                    (q)
+  ;                    '(fun (list (list int)) (list int))
+  ;                    (foldrf foldlf)
+  ;                    (,(list-c '(1 2) '(3 4)))
+  ;                    (list-v 1 2 3 4))
+  ;         '((foldl (flip (foldr cons)) ())))
+  (test "sum"
+        (synthesis 1 (q) '(fun (list int) int) (foldlf) (,(list-c 1 2 3)) (build-num 6))
+        '((foldl add 0)))
   (test "isort"
         (synthesis 1
                    (q)
                    '(fun (list int) (list int))
-                   (noEmptyf sortHelperf ltf fromHeadf)
+                   (noEmptyf sortHelperf fromHeadf)
                    (,(list-c 3 1 2))
                    (list-v 1 2 3))
-        `(,(parser '(fromHead (noEmpty (sortHelper lt)))))))
+        '((fromHead (noEmpty (sortHelper lt)))))
+  (test "adds"
+        (synthesis 1
+                   (q)
+                   '(fun int (fun (list int) (list int)))
+                   (mapf)
+                   ((num ,(build-num 5)) ,(list-c 1 2 3))
+                   (list-v 6 7 8))
+        '((compose map add)))
+  (test "length"
+        (synthesis 1 (q) '(fun (list int) int) (foldrf) (,(list-c 2 2 2)) (build-num 3))
+        '((foldr (const (add 1)) 0)))
+  (test "rember"
+        (synthesis 1
+                   (q)
+                   '(fun int (fun (list int) (list int)))
+                   (filterf)
+                   ((num ,(build-num 2)) ,(list-c 1 2 3))
+                   (list-v 1 3))
+        '((compose filter neq)))
+  (test "maximize"
+        (synthesis 1 (q) '(fun (list int) int) (foldrf) (,(list-c 1 2 3 2 1)) (build-num 3))
+        '((foldr max 0)))
+  (test "minimize"
+        (synthesis 1 (q) '(fun (list int) int) (foldrf) (,(list-c 3 2 5 2 3)) (build-num 2))
+        '((foldr min 0))))
