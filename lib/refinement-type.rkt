285b6203f169906d9f814c381fa910adfc303b7c
mryykt <avrpic9@gmail.com>
2025-12-29 20:45:48 +0900

    feat: 必要な関係をとりあえず定義

--- lib/refinement-type.rkt
+++ lib/refinement-type.rkt
@@ -4,6 +4,7 @@
 (require minikanren/matche)
 (require "utils.rkt")
 
+; env⊢exp:t
 (defmatche (typedo _exp _env _t)
            [((num ,_n) ,_e int)]
            [((char ,_c) ,_e char)]
@@ -26,3 +27,15 @@
            [((cons ,e1 ,e2) ,env ,t)]
            [((car ,e) ,env ,t)]
            [((cdr ,e) ,env ,t)])
+
+; env⊢t
+(defrel (well-formed-typeo t env) (== 1 1))
+
+; env⊢s<:t
+(defrel (subtypingo t s env) (== 1 1))
+
+; env⊢ s=>t
+(defrel (impo t s env) (== 1 1))
+
+; ⊢s:env
+(defrel (closing-substitutiono s env) (== 1 1))
