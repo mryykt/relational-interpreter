48183220842c7d3318dd8fc1f68558794e855ce3
mryykt <avrpic9@gmail.com>
2025-12-31 20:01:09 +0900

    refactor: matcheに変更

--- lib/constraint.rkt
+++ lib/constraint.rkt
@@ -7,18 +7,19 @@
 (provide typedo
          impo)
 
-(defmatche
- (typedo _exp _env _t)
- [((¬ ,e) ,env bool) (typedo e env 'bool)]
- [((,e1 ∧ ,e2) ,env bool) (typedo e1 env 'bool) (typedo e2 env 'bool)]
- [((,e1 = ,e2) ,env bool) (literalo e1) (fresh (t) (typedo e1 env t) (typedo e2 env t))]
- [((,e1 <= ,e2) ,env bool) (literalo e1) (typedo e1 env 'int) (typedo e2 env 'int)]
- [((,e1 + ,e2) ,env int) (literalo e1) (literalo e2) (typedo e1 env 'int) (typedo e2 env 'int)]
- [((,e1 - ,e2) ,env int) (literalo e1) (literalo e2) (typedo e1 env 'int) (typedo e2 env 'int)]
- [((,e1 * ,e2) ,env int) (literalo e1) (literalo e2) (typedo e1 env 'int) (typedo e2 env 'int)]
- [((len ,xs) ,env int) (fresh (t) (symbolo xs) (lookup-firsto xs env `(list ,t)))]
- [(,n ,_e int) (numbero n)]
- [(,x ,env ,t) (symbolo x) (lookup-firsto x env t)])
+(defrel
+ (typedo _exp env _t)
+ (matche (_exp _t)
+         [((¬ ,e) bool) (typedo e env 'bool)]
+         [((,e1 ∧ ,e2) bool) (typedo e1 env 'bool) (typedo e2 env 'bool)]
+         [((,e1 = ,e2) bool) (literalo e1) (fresh (t) (typedo e1 env t) (typedo e2 env t))]
+         [((,e1 <= ,e2) bool) (literalo e1) (typedo e1 env 'int) (typedo e2 env 'int)]
+         [((,e1 + ,e2) int) (literalo e1) (literalo e2) (typedo e1 env 'int) (typedo e2 env 'int)]
+         [((,e1 - ,e2) int) (literalo e1) (literalo e2) (typedo e1 env 'int) (typedo e2 env 'int)]
+         [((,e1 * ,e2) int) (literalo e1) (literalo e2) (typedo e1 env 'int) (typedo e2 env 'int)]
+         [((len ,xs) int) (fresh (t) (symbolo xs) (lookup-firsto xs env `(list ,t)))]
+         [(,n int) (numbero n)]
+         [(,x ,t) (symbolo x) (lookup-firsto x env t)]))
 
 (defrel (literalo exp) (matche exp [(len ,e) (symbolo e)] [,n (numbero n)] [,x (symbolo x)]))
 
