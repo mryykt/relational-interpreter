f2c2ae569388bb848d1efaff1dbddcb5d75eff6f
mryykt <avrpic9@gmail.com>
2026-01-15 16:32:26 +0900

    feat: testの出力を整えた

--- lib/test-check.rkt
+++ lib/test-check.rkt
@@ -10,15 +10,24 @@
   (syntax-rules ()
     [(_ title tested-expression expected-result)
      (begin
-       (printf "Testing ~s\n" title)
+       (printf "Testing ~s " title)
+       (flush-output)
        (let* ([expected expected-result]
-              [produced (with-handlers ([exn:fail:resource?
-                                         (λ (e) (displayln (format "Timeout in ~s" timeout-second)))])
-                          (call-with-limits timeout-second #f (λ () (time tested-expression))))])
-         (or (equal? expected produced)
-             (begin
-               (test-failed #t)
-               (printf "Failed: ~s~%Expected: ~s~%Computed: ~s~%"
-                       'tested-expression
-                       expected
-                       produced)))))]))
+              [produced
+               (with-handlers ([exn:fail:resource? (λ (e) (display (format "~s s " timeout-second)))])
+                 (call-with-limits timeout-second
+                                   #f
+                                   (λ ()
+                                     (begin
+                                       (define start (current-milliseconds))
+                                       (define ret tested-expression)
+                                       (define end (current-milliseconds))
+                                       (printf "~s ms " (- end start))
+                                       ret))))])
+         (cond
+           [(void? produced) (printf "\e[31m×\e[0m\n")]
+           [(equal? expected produced) (printf "\e[32m✔\e[0m\n")]
+           [else
+            (begin
+              (test-failed #t)
+              (printf "\e[31m×\e[0m Expected: ~s~%Computed:" expected produced))])))]))
